a6e52400f70ec49404712f45f01d1a533056995c
Aymeric Aitamer <aymeric@kohort.eu>
2024-01-18 11:07:58 +0100

    Improve Dockerfile for Heroku

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN mkdir -p /docker-entrypoint-init.d
 COPY magento-init.sh /docker-entrypoint-init.d/magento-init.sh
 RUN chmod +x /docker-entrypoint-init.d/magento-init.sh
 
-
 # Fix Mutex posixsem for Mac M1
 RUN echo 'Mutex posixsem' >> /opt/bitnami/apache/conf/httpd.conf
+
+# Use PORT env variable for Heroku
+RUN sed -i "s/8080/\${PORT}/g" /opt/bitnami/apache/conf/httpd.conf
+RUN sed -i "s/APACHE_HTTP_PORT_NUMBER/PORT/g" /opt/bitnami/scripts/apache/setup.sh /opt/bitnami/scripts/apache-env.sh /opt/bitnami/scripts/libapache.sh
---
+++ Kohortpay/Payment/Model/Payment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kohortpay\Payment\Model;
+
+/**
+ * Pay In Store payment method model
+ */
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+  /**
+   * Payment code
+   *
+   * @var string
+   */
+  protected $_code = 'kohortpay';
+
+  /**
+   * Authorizes specified amount.
+   *
+   * @param InfoInterface $payment
+   * @param float         $amount
+   *
+   * @return $this
+   *
+   * @throws LocalizedException
+   */
+  public function authorize(
+    \Magento\Payment\Model\InfoInterface $payment,
+    $amount
+  ) {
+    return $this;
+  }
+}
